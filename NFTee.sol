14f8ed521c82f0bd73d115f6f3c8279838681e5f
Pavan Tamrakar <72463719+PavanTamrakar@users.noreply.github.com>
2022-08-21 13:07:52 +0530

    Create NFTee.sol

---
+++ NFTee.sol
@@ -0,0 +1,12 @@
+//SPDX-Licence-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFTee is ERC721 {
+
+    constructor () ERC721 ("NFT721-ERC", "NFT721") {
+        _mint(msg.sender, 1);
+    }
+    
+}
